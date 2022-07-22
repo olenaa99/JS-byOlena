c2f5c322ce50083da2974ff480a35d497c262983
olenaa99 <o.havrylyshyn99@gmail.com>
2022-07-22 15:40:34 +0300

    hw

---
+++ lesson1/main.js
@@ -0,0 +1,95 @@
+let a1="hello";
+    console.log(a1)
+//alert(a1);
+document.write (a1)
+
+let a2="owu";
+    console.log(a2)
+//alert(a2);
+document.write(a2)
+
+let a3="com";
+    console.log(a3)
+//alert(a3);
+document.write(a3)
+
+let a4="ua";
+    console.log(a4)
+//alert(a4);
+document.write(a4)
+
+let b1=1;
+    console.log(b1)
+//alert(a5);
+document.write(b1)
+
+let b2=10;
+    console.log(b2)
+//alert(b2);
+document.write(b2)
+
+let b3=-999;
+    console.log(b3)
+//alert(b3);
+document.write(b3)
+
+let b4=123;
+    console.log(b4)
+//alert(b4);
+document.write(b4)
+
+let b5=3.14;
+    console.log(b5)
+//alert(b5);
+document.write(b5)
+
+let b6=2.7;
+    console.log(b6)
+//alert(b6);
+document.write(b6)
+
+let b7=16;
+    console.log(b7)
+//alert(b7);
+document.write(b7)
+
+let b8=true;
+    console.log(b8)
+//alert(b8);
+document.write(b8)
+
+let b9=false;
+    console.log(b9)
+//alert(b9);
+document.write(b9)
+
+
+let firstName ='Гаврилишин'
+let middleName = 'Олена'
+let lastName = 'Миколаївна'
+let person= firstName+ ' '+ middleName + ' '+ lastName;
+console.log(person);
+
+
+
+
+
+let a=100;
+    console.log(typeof a)
+let b='100';
+    console.log(typeof b)
+let c=true;
+    console.log(typeof c)
+
+
+let f1='Olena';
+    console.log(!!f1)
+    let inputdata1 = prompt(!!f1)
+
+let f2='Havrylyshyn';
+    console.log(!!f2)
+    let inputdata2= prompt(!!f2)
+
+let f3='Mykolaivna';
+    console.log(!!f3)
+    let inputdata3= prompt(!!f3)
